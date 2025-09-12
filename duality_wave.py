532d077898d3a707666689206a57015719e0ebcc
halfdane <halfdane@gmx.net>
2025-09-12 21:15:24 +0200

    with knurl

--- duality_wave.py
+++ duality_wave.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import math
 import copy
 from build123d import *
@@ -18,7 +18,7 @@ class KeyCol:
 class PinkieDimensions(KeyCol):
     x: float = 0
     y: float = 0
-    offset: float = -2
+    offset_x: float = -1
     rotation: float = 8
     locs: list[Location] = (
         Location((0, 0)), 
@@ -28,7 +28,7 @@ class PinkieDimensions(KeyCol):
 
 @dataclass
 class RingFingerDimensions(KeyCol):
-    x: float = Choc.cap.width_x + PinkieDimensions.offset
+    x: float = Choc.cap.width_x + PinkieDimensions.offset_x
     y: float = 14
     rotation: float = 0
     locs: list[Location] = (
@@ -39,7 +39,7 @@ class RingFingerDimensions(KeyCol):
 
 @dataclass
 class MiddleFingerDimensions(KeyCol):
-    x: float = 2*Choc.cap.width_x + PinkieDimensions.offset
+    x: float = 2*Choc.cap.width_x + PinkieDimensions.offset_x
     y: float = 22.4
     rotation: float = 0
     locs: list[Location] = (
@@ -50,7 +50,7 @@ class MiddleFingerDimensions(KeyCol):
 
 @dataclass
 class PointerFingerDimensions(KeyCol):
-    x: float = 3*Choc.cap.width_x + PinkieDimensions.offset
+    x: float = 3*Choc.cap.width_x + PinkieDimensions.offset_x
     y: float = 18
     rotation: float = 0
     locs: list[Location] = (
@@ -61,7 +61,7 @@ class PointerFingerDimensions(KeyCol):
 
 @dataclass
 class InnerFingerDimensions(KeyCol):
-    x: float = 4*Choc.cap.width_x + PinkieDimensions.offset
+    x: float = 4*Choc.cap.width_x + PinkieDimensions.offset_x
     y: float = 14
     rotation: float = 0
     locs: list[Location] = (
@@ -71,21 +71,20 @@ class InnerFingerDimensions(KeyCol):
     )
 
 @dataclass
+class ThumbDimensions(KeyCol):
+    x: float = InnerFingerDimensions.locs[0].position.X - 6.5
+    y: float = InnerFingerDimensions.locs[0].position.Y - 18
+    rotation: float = -8
+    locs: list[Location] = (
+        Location((x, y)), 
+        Location((Choc.cap.width_x + x, y + Choc.cap.length_y/rotation)), 
+    )
+
+@dataclass
 class CaseDimensions:
     clearance: float = 0.02
     wall_thickness: float = 1.3
     pin_radius: float = 0.5
-    magnet_radius: float = 5
-    magnet_thickness_z: float = 2
-    weight_x: float = 19
-    weight_y: float = 11.5
-    weight_z: float = 4
-    battery_x: float = 31
-    battery_y: float = 17
-    battery_z: float = 5.5
-
-    shadow_line_height_z: float = 0.3
-    shadow_line_depth_x: float = 0.3
 
     pattern_depth: float = 0.2
 
@@ -100,36 +99,146 @@ class DualityWaveCase:
     middle = MiddleFingerDimensions()
     pointer = PointerFingerDimensions()
     inner = InnerFingerDimensions()
+    thumb = ThumbDimensions()
+    keycols = [pinkie, ring, middle, pointer, inner, thumb]
 
     def __init__(self, with_knurl=False, debug=False):
         self.with_knurl = with_knurl
         self.debug = debug
 
+        self.wall_height = Choc.bottom_housing.height_z + Choc.posts.post_height_z
+
         self.create_case()
 
+        self.create_accessories()
+
     def create_case(self):
+        print("Creating case...")
+
+        with BuildSketch() as key_holes:
+            for keycol in self.keycols:
+                with Locations(*keycol.locs):
+                    Rectangle(Choc.bottom_housing.width_x, Choc.bottom_housing.depth_y, rotation=keycol.rotation)
+            push_object(key_holes, name="key_holes") if self.debug else None
+            
+        with BuildSketch() as outline:
+            base_width_x = 107
+            base_length_y = 85
+            thumb_container_height_y = 25
+            circle_radius = 39.9
+
+            with Locations((base_width_x/2 - 21.5, base_length_y/2 - 12.9)):
+                Rectangle(base_width_x, base_length_y)
+            with Locations((self.thumb.locs[1].position.X-10, self.thumb.locs[1].position.Y + thumb_container_height_y/2 + 1.5)):
+                Rectangle(2*Choc.cap.width_x + 10, thumb_container_height_y + 21.5, rotation=self.thumb.rotation)
+            with Locations((base_width_x + circle_radius - 21.5, self.thumb.locs[1].position.Y + 35.5)):
+                Circle(circle_radius, mode=Mode.SUBTRACT)
+
+            outline = outline.sketch
+
+            push_object(outline, name="outline") if self.debug else None
+
         with BuildPart() as self.case:
             self.case.name = "Case"
 
-            plate = Box(200, 200, self.dims.wall_thickness, mode=Mode.PRIVATE)
-            plate = plate.translate((80, 90, -self.dims.wall_thickness/2))
-            add(plate)
-            
-            
-            with BuildSketch() as outline:
-                for keycol in [self.pinkie, self.ring, self.middle, self.pointer, self.inner]:
-                    with Locations(*keycol.locs):
-                        Rectangle(Choc.bottom_housing.width_x, Choc.bottom_housing.depth_y, rotation=keycol.rotation)
+            with BuildSketch() as walls:
+                add(outline)
+            extrude(amount=-self.wall_height-self.dims.wall_thickness)
+
+            if self.with_knurl: 
+                print("Adding knurl...")
+                tops = self.case.faces().filter_by(Axis.Z)
+                walls = self.case.faces().filter_by(lambda f: f not in tops).sort_by(Axis.X, reverse=True)
+                self.create_knurl(distance=10 if self.debug else 1.5, width_x=200, height_y=50)
+                for i, wall in enumerate(walls):
+                    print(f"  Projecting pattern to wall {i+1} of {len(walls)}")
+                    push_object(wall, name=f"wall_{i}") if self.debug else None
+                    faces, pattern = self.project_to_face(self.knurl.rotate(Axis.X, 90), wall)
+                    push_object(pattern, name=f"pattern_{i}") if self.debug else None
+                    for i, face in enumerate(faces):
+                        thicken(to_thicken=face, amount=-self.dims.pattern_depth, mode=Mode.SUBTRACT)
+
+            with BuildSketch(Plane.XY.offset(-self.dims.wall_thickness-self.wall_height)) as keep_walls:
+                    offset(
+                            outline,
+                            -self.dims.wall_thickness,
+                            kind=Kind.INTERSECTION,
+                        )
+            extrude(amount=self.dims.wall_thickness, mode=Mode.SUBTRACT)
+            push_object(keep_walls, name="keep_walls") if self.debug else None
+
+            with BuildSketch() as keyholes:
+                add(key_holes)
             extrude(amount=-self.dims.wall_thickness, mode=Mode.SUBTRACT)
-            if (self.debug):
-                push_object(outline, name="outline")
+            with BuildSketch(Plane.XY.offset(-self.dims.wall_thickness)) as keyholes:
+                offset(
+                        key_holes.sketch,
+                        1, 
+                        kind=Kind.INTERSECTION
+                )
+            extrude(amount=-self.wall_height, mode=Mode.SUBTRACT)
+            push_object(key_holes, name="keyholes") if self.debug else None
 
-            choc = Choc()
-            for keycol in [self.pinkie, self.ring, self.middle, self.pointer, self.inner]:
-                with Locations(*keycol.locs):
+
+
+    def project_to_face(self, pattern, face: Face) -> Vector:
+        vector = face.normal_at((0,0,0))
+
+        abs_vector = Vector(abs(vector.X), abs(vector.Y), abs(vector.Z))
+        if abs_vector.X >= abs_vector.Y and abs_vector.X >= abs_vector.Z:
+            direction, pattern = (Vector(-1, 0, 0), pattern.rotate(Axis.Z, 90).translate((200, 0, 0))) if vector.X >= 0 else (Vector(1, 0, 0), pattern.rotate(Axis.Z, 90).translate((-200, 0, 0)))
+        elif abs_vector.Y >= abs_vector.X and abs_vector.Y >= abs_vector.Z:
+            direction, pattern = (Vector(0, -1, 0), pattern.translate((0, 200, 0))) if vector.Y >= 0 else (Vector(0, 1, 0), pattern.translate((0, -200, 0)))
+        else:
+            direction, pattern = (Vector(0, 0, -1), pattern.translate((0, 0, 200))) if vector.Z >= 0 else (Vector(0, 0, 1), pattern.translate((0, 0, -200)))
+        
+        return pattern.face().project_to_shape(face, direction).faces(), pattern
+
+
+    def create_knurl(self, width_x=0, height_y=0, distance=2):
+        pattern_width=distance
+        scale=2/3
+        half_height = pattern_width / math.atan(math.radians(90))
+        points = [
+            (scale*-pattern_width/2, 0),
+            (0, -scale*half_height),
+            (scale*pattern_width/2, 0),
+            (0, scale*half_height),
+        ]
+        with BuildSketch() as rhombus:
+            Polygon(*points)
+
+        with BuildSketch() as pattern:
+            Rectangle(width_x, height_y)
+            with Locations((0,0), (-pattern_width/2, half_height)):
+                with GridLocations(
+                    x_count=math.ceil(width_x/pattern_width)+1,
+                    y_count=math.ceil(height_y/(2*half_height))+1,
+                    x_spacing=pattern_width,
+                    y_spacing=2*half_height,
+                ):
+                    add(rhombus, mode=Mode.SUBTRACT)
+
+        self.knurl = pattern.sketch
+        self.knurl.name = "Surface Pattern"
+        self.knurl.distance = distance
+
+
+    def create_accessories(self):
+        choc = Choc()
+        with BuildPart() as self.chocs:
+            self.chocs.name = "Choc Switches"
+
+            for keycol in self.keycols:
+                with Locations([loc*Location((0,0,Choc.base.thickness_z)) for loc in keycol.locs]):
                     add(copy.copy(choc.model.part).rotate(Axis.Z, keycol.rotation))
+        push_object(self.chocs, name="chocs")
 
-
+        xiao = Xiao()
+        xiao.model = xiao.model.part\
+            .rotate(Axis.X, 180)\
+            .translate((-5, 62, -self.dims.wall_thickness))
+        push_object(xiao.model, name="xiao")
 
             
 
@@ -139,8 +248,8 @@ if __name__ == "__main__":
     set_defaults(ortho=True, default_edgecolor="#121212", reset_camera=Camera.KEEP)
     set_colormap(ColorMap.seeded(colormap="rgb", alpha=1, seed_value="wave"))
 
-    knurl = False
+    knurl = True
     case = DualityWaveCase(with_knurl=knurl, debug=True)
 
     push_object(case.case) if hasattr(case, "case") else None
-    show_objects()
+    show_objects() 
--- holder.py
+++ holder.py
@@ -1,44 +1,7 @@
 #!/usr/bin/env python3
+import math
 from build123d import *
 
-def create_snaking_lines(distance=2, rotation_angle=0, width_x=0, height_y=0):    
-    face_width=width_x
-    face_height=height_y
-
-    with BuildLine() as line:
-        y_positions = [-face_height/2 + i * distance for i in range(-1, int(face_height // distance) + 2)]
-
-        polyline_points = []
-        for i, y in enumerate(y_positions):
-            if i % 2 == 0:  # Even rows: left to right
-                start_point = (-face_width, y)
-                end_point = (face_width, y)
-            else:  # Odd rows: right to left
-                start_point = (face_width, y)
-                end_point = (-face_width, y)
-
-            polyline_points.extend([start_point, end_point])
-        
-        FilletPolyline(*polyline_points, radius=distance/3)
-    snake_face = offset(line.line, 0.2*distance)
-    with BuildSketch() as sketch:
-        add(snake_face.rotate(Axis.Z, rotation_angle))
-        make_face()
-
-        add(snake_face.rotate(Axis.Z, -rotation_angle))
-        make_face()
-
-    return sketch.sketch
-
-def the_line_test():
-    with BuildLine() as heyy:
-        r = Line((0,0,0), (0,0,20))
-        s = Line((0,12,0), (0,0,-20))
-        add(r)
-        add(s)
-        add(Line(r@0, s@0))
-    lol = offset(heyy.line, 7)
-    return lol
 
 if __name__ == "__main__":
     import time
@@ -55,34 +18,30 @@ if __name__ == "__main__":
     width_x=44.1
     height_y=57.9
     depth_z=20
-    snake = create_snaking_lines(distance=2, rotation_angle=30, width_x=width_x, height_y=height_y)
+    
+    
+    pattern_width=1.5
+    scale=2/3
+    half_height = pattern_width / math.atan(math.radians(90))
+    points = [
+        (scale*-pattern_width/2, 0),
+        (0, -scale*half_height),
+        (scale*pattern_width/2, 0),
+        (0, scale*half_height),
+    ]
+    with BuildSketch() as rhombus:
+        Polygon(*points)
 
-    lol = the_line_test()
-
-    x_offset = -4
-    y_offset = 10
-    z_offset = 7
-
-    def chamfer_this(edge: Edge):
-        start = edge@0
-        end = edge@1
-        return \
-            start.X == end.X == (x_offset - width_x/2) and \
-            start.Y != end.Y and \
-            start.Z != end.Z
-
-    thing_cutter = snake\
-        .rotate(Axis.Y, 90)\
-        .translate((x_offset - width_x/2, y_offset, z_offset))
-    with BuildPart() as thing:
-        with Locations((x_offset, y_offset, z_offset)):
-            Box(width_x, height_y, depth_z)
-        cutface = thing.faces().sort_by(Axis.X)[0]
-        extrude(thing_cutter, amount=1, mode=Mode.SUBTRACT)
-
-
-
-    total_time = time.time() - start_time
-    print(f"Total execution time: {total_time:.3f}s")
+    with BuildSketch() as pattern:
+        Rectangle(width_x, height_y)
+        with Locations((0,0), (-pattern_width/2, half_height)):
+            with GridLocations(
+                x_count=math.ceil(width_x/pattern_width)+1,
+                y_count=math.ceil(height_y/(2*half_height))+1,
+                x_spacing=pattern_width,
+                y_spacing=2*half_height,
+            ):
+                add(rhombus, mode=Mode.SUBTRACT)
+        
 
     show_all()
